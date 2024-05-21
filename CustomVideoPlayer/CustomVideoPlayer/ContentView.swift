f0b3461382cb9d5a3eb73dfdfe0f9a52c3b2719d
“amarasagekar” <amarasagekar@gmail.com>
2024-05-21 21:59:47 +0530

    project setup

---
+++ CustomVideoPlayer/CustomVideoPlayer/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  CustomVideoPlayer
+//
+//  Created by AMAR on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+            .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomVideoPlayer/CustomVideoPlayer/CustomVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomVideoPlayerApp.swift
+//  CustomVideoPlayer
+//
+//  Created by AMAR on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
